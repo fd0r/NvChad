7c3f71dd5244bcf5207ffa2113ce211e2029bba6
Luis Montero <luismontero@hotmail.fr>
2023-08-22 11:09:00 +0200

    comment stuff

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -14,7 +14,7 @@
 -- Big hack to get the correct env for the lsp
 local conda_env = os.getenv "CONDA_PREFIX"
 if conda_env then
-  print(conda_env)
+  -- print(conda_env)
   vim.g.python3_host_prog = conda_env .. "/bin/python"
 end
 
@@ -33,7 +33,7 @@ local setup_python_for_zama = function()
     if not action_triggered and vim.bo.filetype == "python" then
       -- Perform your desired action here
       -- For example, you can call a function or run a command
-      vim.api.nvim_command ":PyrightSetPythonPath /Users/luismontero/miniforge3/envs/zama/bin/python"
+      -- vim.api.nvim_command ":PyrightSetPythonPath /Users/luismontero/miniforge3/envs/zama/bin/python"
       -- Set the flag to true to indicate that the action has been triggered
       action_triggered = true
     end
@@ -43,9 +43,9 @@ end
 local conda_env_path = os.getenv "CONDA_PREFIX"
 local zama_root_dir = vim.fs.dirname(vim.fs.find({ "zama" }, { upward = true })[0])
 if conda_env_path then
-  print "in zama conda env"
+  -- print "in zama conda env"
   setup_python_for_zama()
 elseif zama_root_dir then
-  print "in zama work dir"
+  -- print "in zama work dir"
   setup_python_for_zama()
 end
